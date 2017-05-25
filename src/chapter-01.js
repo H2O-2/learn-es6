3330b85e8a532594d57bc0690f9c1da015483fd3
H2O-2 <h2o-2@qq.com>
2017-05-25 10:00:28 -0400

    chapter 1

--- src/chapter-01.js
+++ src/chapter-01.js
@@ -52,6 +52,24 @@ if (true) {
 
 
 
+//const实际上保证的，并不是变量的值不得改动，而是变量指向的那个内存地址不得改动
+const constTest = {};
+constTest.field1 = 1; // this works
+//constTest = {}; This does not work
+
+
+
+// var命令和function命令声明的全局变量，依旧是顶层对象的属性；另一方面规定，let命令、const命令、class命令声明的全局变量，不属于顶层对象的属性。
+var globalTest = 1;
+console.log(window.globalTest); // 1
+let globalTest2 = 1;
+console.log(window.globalTest2); // undefined
+
+
+
+
+
+
 
 
 
